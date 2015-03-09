d8718344002cf51a2c05ac3922334f52a647ea84
zhenlongl <742946057@qq.com>
2015-03-09 16:28:50 +0800

    up

--- apps/client/application/views/user/register.php
+++ apps/client/application/views/user/register.php
@@ -8,15 +8,15 @@
 			<div class="am-fl am-cf"><strong class="am-text-primary am-text-lg">首页</strong> /用户注册 <small></small></div>
 		</div>
 		<div class="am-tab-panel">
-			<form class="am-form">
+			<form class="am-form" onsubmit="return false;">
 				<div class="am-g am-margin-top">
 					<div class="am-u-sm-4 am-u-md-2 am-text-right">
 						用户名
 					</div>
-					<div class="am-u-sm-8 am-u-md-4">
-						<input type="text" class="am-input-sm">
+					<div class="am-u-sm-8 am-u-md-4 ">
+						<input type="text" class="am-input-sm" name="user_name" id="user_name">
 					</div>
-					<div class="am-hide-sm-only am-u-md-6">*必填，不可重复</div>
+					<div class="am-u-sm-12 am-u-md-6" style="display: none;" id="user_name_tips"></div>
 				</div>
 
 				<div class="am-g am-margin-top">
@@ -24,8 +24,9 @@
 						邮箱
 					</div>
 					<div class="am-u-sm-8 am-u-md-4 am-u-end col-end">
-						<input type="text" class="am-input-sm">
+						<input type="text" class="am-input-sm" name="email" id="email">
 					</div>
+					<div class="am-u-sm-12 am-u-md-6" style="display: none;" id="email_tips"></div>
 				</div>
 
 				<div class="am-g am-margin-top">
@@ -33,36 +34,32 @@
 						密码
 					</div>
 					<div class="am-u-sm-8 am-u-md-4">
-						<input type="text" class="am-input-sm">
+						<input type="password" class="am-input-sm" name="password" id="password">
 					</div>
-					<div class="am-hide-sm-only am-u-md-6">选填</div>
+					<div class="am-u-sm-12 am-u-md-6" style="display: none;" id="password_tips"></div>
 				</div>
-
 				<div class="am-g am-margin-top">
 					<div class="am-u-sm-4 am-u-md-2 am-text-right">
 						确认密码
 					</div>
 					<div class="am-u-sm-8 am-u-md-4">
-						<input type="text" class="am-input-sm">
+						<input type="password" class="am-input-sm" name="password2" id="password2">
 					</div>
-					<div class="am-u-sm-12 am-u-md-6">不填写则自动截取内容前255字符</div>
+					<div class="am-u-sm-12 am-u-md-6" style="display: none;" id="password2_tips"></div>
 				</div>
-
-				<!--<div class="am-g am-margin-top-sm">
-					<div class="am-u-sm-12 am-u-md-2 am-text-right admin-form-text">
-						内容描述
+				<div class="am-g am-margin-top am-margin-bottom">
+					<div class="am-u-sm-2 am-u-sm-offset-2">
+					<button type="button" class="am-btn am-btn-primary am-btn-block" id="submit_register">提交保存</button>
 					</div>
-					<div class="am-u-sm-12 am-u-md-10">
-						<textarea rows="10" placeholder="请使用富文本编辑插件"></textarea>
-					</div>
-				</div>-->
-
+				</div>
 			</form>
 		</div>
 	</div>
 </div>
 <!-- content end -->
 <a class="am-icon-btn am-icon-th-list am-show-sm-only admin-menu" data-am-offcanvas="{target: '#admin-offcanvas'}"></a>
+<script src="<?php echo base_url('assets/js/public.js');?>"></script>
+<script src="<?php echo base_url('assets/js/user/user.js');?>"></script>
 <?php
 	$this->load->view('web_public/footer');
 ?>
