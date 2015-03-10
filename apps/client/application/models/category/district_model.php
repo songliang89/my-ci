1399beb0b36330e692ccd5cf4fd2315d2f98126e
zhenlongl <742946057@qq.com>
2015-03-10 18:28:33 +0800

    add 表结构

--- apps/client/application/models/category/district_model.php
+++ apps/client/application/models/category/district_model.php
@@ -5,6 +5,20 @@
  * Date: 15/3/10
  * Time: 下午5:31
  */
+
+/**
+ *
+ * CREATE TABLE `category_district` (
+ *      `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ *      `parentid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '父级分类id',
+ *      `category_name` varchar(30) NOT NULL DEFAULT '' COMMENT '分类名称',
+ *      `category_order` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+ *      `is_delete` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '是否删除',
+ *      `level` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '级别',
+ *      PRIMARY KEY (`id`)
+ * ) ENGINE=InnoDB AUTO_INCREMENT=538 DEFAULT CHARSET=utf8
+ *
+ */
 class District_model extends MY_Model
 {
 	function __construct()
