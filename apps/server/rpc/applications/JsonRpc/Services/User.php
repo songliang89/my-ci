53baf5b0294829b7ea13a4e33f93652a1c845986
zhenlongl <742946057@qq.com>
2015-03-13 15:45:05 +0800

    RPC 加database orm eloquent

---
+++ apps/server/rpc/applications/JsonRpc/Services/User.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *  测试
+ * @author walkor <worker-man@qq.com>
+ */
+
+class User_model extends Illuminate\Database\Eloquent\Model
+{
+   protected $table = 'web_user';
+
+}
+
+class User
+{
+	static function getUser()
+	{
+		//return "aaa";
+		$data = User_model::all();
+		return $data;
+	}
+}
